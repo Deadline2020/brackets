fa66fd5804be2458d74c1c013912612c667fdfc2
Andrew <Deadline2020@yandex.by>
2019-10-01 02:08:02 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let resultArr = [];
+    for (let char of str) {
+        let resultFind = bracketsConfig.find(item => item[1] === char);
+        if (resultFind) {
+            if (resultArr[resultArr.length - 1] === resultFind[0]) resultArr.pop()
+            else resultArr.push(char)
+        }
+        else resultArr.push(char)
+    }
+    return !resultArr.length;
 }
